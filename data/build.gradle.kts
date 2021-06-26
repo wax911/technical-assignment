29f1f8015c4627144da448470e48f7f8bd7985b8
Maxwell <mxt.developer@gmail.com>
2021-06-26 11:34:42 +0200

    Update missing json plugins and configuration

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,9 +1,21 @@
-import za.co.itschools.dev.buildSrc.Libraries 
+import za.co.itschools.dev.buildSrc.Libraries
+import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
 
 plugins {
 	id("za.co.itschools.dev.plugin")
+	id("kotlinx-serialization")
+}
+
+tasks.withType(KotlinCompile::class.java) {
+	kotlinOptions {
+		freeCompilerArgs = listOf(
+			"-Xopt-in=kotlinx.serialization.ExperimentalSerializationApi",
+			"-Xopt-in=kotlin.RequiresOptIn"
+		)
+	}
 }
 
 dependencies {
 	implementation(Libraries.AndroidX.Preference.preferenceKtx)
+	implementation(Libraries.JetBrains.KotlinX.Serialization.json)
 }
